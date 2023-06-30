88690963098ddc95f52463b2b46152abe629aafd
glen Hughes <gl3nhugh3s@gmail.com>
2023-06-30 14:39:58 -0700

    first commit to working branch

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen p-24">
       <h1>nextjs webscraper starter point</h1>
+      <h2>this is the starting point of the working branch</h2>
     </main>
   )
 }
