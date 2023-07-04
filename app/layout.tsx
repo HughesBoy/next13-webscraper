0feff9a19d8aab74b90c26fb4db27465cd6e7eff
glen Hughes <gl3nhugh3s@gmail.com>
2023-07-04 11:47:14 -0700

    basic webscraper working

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,4 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'web scraper with nextjs13 app router + typescript',
@@ -15,7 +12,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className="bg-zinc-900">{children}</body>
     </html>
   )
 }
